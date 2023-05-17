7c1bee30cf23437a77ac2ae04d4792f72d99f407
Dev Agrawal <dev@clerk.dev>
2023-05-17 18:05:20 -0400

    bolded signup with google to make the color change more noticable

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
             "bg-black border border-black border-solid hover:bg-white hover:text-black",
           socialButtonsBlockButton:
             "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
-          socialButtonsBlockButtonArrow: "text-black",
+          socialButtonsBlockButtonText: "font-semibold",
           formButtonReset:
             "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
           membersPageInviteButton:
