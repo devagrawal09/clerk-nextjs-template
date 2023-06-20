d5c4bec7df541a5275eeced495cdb980c51ec436
Dev Agrawal <dev@clerk.dev>
2023-06-20 14:48:10 -0400

    disabled toggle button

--- app/dashboard/details.tsx
+++ app/dashboard/details.tsx
@@ -244,7 +244,7 @@ export function OrgDetails() {
               </dl>
             )
           ) : (
-            <div className="text-gray-700 px-4 py-5">
+            <div className="text-gray-700 px-4 py-5 text-sm">
               You are currently logged in to your personal workspace.
               <br />
               Create or switch to an organization to see its details.
@@ -274,6 +274,7 @@ function Toggle(props: {
           "rounded-l py-2 px-4 border-solid border border-gray-300 transition text-sm font-semibold":
             true,
           "bg-gray-100": !props.checked,
+          "bg-gray-50 text-gray-500 cursor-not-allowed": props.disabled,
         })}
       >
         List
@@ -285,6 +286,7 @@ function Toggle(props: {
           "rounded-r py-2 px-4 border-solid border border-gray-300 -ml-[1px] transition text-sm font-semibold":
             true,
           "bg-gray-100": props.checked,
+          "bg-gray-50 text-gray-500 cursor-not-allowed": props.disabled,
         })}
       >
         JSON
