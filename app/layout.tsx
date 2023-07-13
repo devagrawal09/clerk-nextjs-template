1eb54c9554267b9467cb9f9ffcf3e02662b2e601
Dev Agrawal <dev@clerk.dev>
2023-07-12 21:13:18 -0400

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,15 @@ import Script from "next/script";
 import { Docs, Github, Times } from "./icons";
 import { Twitter } from "./icons";
 import { Discord } from "./icons";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Next.js Clerk Template",
   description:
     "A simple and powerful Next.js template featuring authentication and user management powered by Clerk.",
+  openGraph: { images: ["/og.png"] },
 };
 
 export default function RootLayout({
